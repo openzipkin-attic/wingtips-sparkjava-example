4e32c4cffc285112dfa7d7694b4cd7eaa68844a2
Janusz Piechówka <januszpiechowka@gmail.com>
2016-08-20 14:03:33 +0200

    Upload files

---
+++ src/CujojsClient.js
@@ -0,0 +1,30 @@
+const {Tracer, BatchRecorder, ExplicitContext} = require('zipkin');
+const {HttpLogger} = require('zipkin-transport-http');
+const {restInterceptor} = require('zipkin-instrumentation-cujojs-rest');
+
+const rest = require('rest');
+
+const ctxImpl = new ExplicitContext();
+const recorder = new BatchRecorder({
+    logger: new HttpLogger({
+        endpoint: 'http://localhost:9411/api/v1/spans'
+    })
+});
+
+const tracer = new Tracer({ctxImpl, recorder});
+const nameOfRemoteService = 'cujojs-client';
+const client = rest.wrap(restInterceptor, {tracer, serviceName: nameOfRemoteService});
+
+client({
+    method: 'GET',
+    path: 'http://localhost:9999/request'
+}).then(function(response) {
+    console.log('response: ', response);
+
+    //TODO: FIX
+    setTimeout(function () {
+        console.log('timeout');
+    }, 5000);
+
+
+});
